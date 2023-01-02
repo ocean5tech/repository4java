a984e8e98905d61c0b7bbbd098bf6fe3eba4e40b
ocean5apphotmail <ocean5app@hotmail.com>
2023-01-02 14:10:17 +0800

    before add dependency

---
+++ Spring Boot Complete Tutorial - Master Class/SpringFrameworkDemo/src/main/java/Demo/Doctor.java
@@ -0,0 +1,13 @@
+package Demo;
+
+/* @Auther: wuyang
+ * @Date: 2023/01/02/14:00
+ * @Description:
+ */
+public class Doctor {
+
+    public void assist(){
+      System.out.println("Doctor is assisting");
+    }
+
+}
--- Spring Boot Complete Tutorial - Master Class/SpringFrameworkDemo/src/main/java/Demo/Main.java
+++ Spring Boot Complete Tutorial - Master Class/SpringFrameworkDemo/src/main/java/Demo/Main.java
@@ -5,4 +5,8 @@ package Demo;
  * @Description:
  */
 public class Main {
+    public static void main(String[] args) {
+        Doctor doctor = new Doctor();
+        doctor.assist();
+    }
 }
